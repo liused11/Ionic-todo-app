ae42bb22b16e7ced2cdfdb28f8f0a5ec4f66aafa
liused11 <burinrachakitja@gmail.com>
2025-05-14 03:54:33 +0700

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://fqhjzugzrfbjhhaivptc.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxaGp6dWd6cmZiamhoYWl2cHRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxMjMzMzcsImV4cCI6MjA2MjY5OTMzN30.eh3-NH6mLOsoo00P3gaNTnUBacf8w2l0313wrzeG80o'
 };
